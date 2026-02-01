962e53bc69e6225bbd6672cd956b6ca9353d9806
David <dgios@kth.se>
2026-02-01 12:59:45 +0100

    Update functions.c control function

--- BoardA/Core/Src/functions.c
+++ BoardA/Core/Src/functions.c
@@ -22,17 +22,35 @@ void motor_turnOn(void){
 }
 
 // Applies specific duty cycle to the motor
-void motor_applyControl(uint32_t control){
+void motor_applyControl(int8_t cmd){
 
-	control = 1<<30;
-	if(control > 65535) control = 65535;
-
-	uint8_t reverse = 0;
-	if(reverse){
-		__HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1, control);
-	} else {
-		__HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_2, control);
-	}
+	// Clamp input 
+	if (cmd > 100)  cmd = 100;
+    if (cmd < -100) cmd = -100;
+	
+	uint32_t duty = 0;
+	
+    // Convert percentage to timer counts
+	// cmd is transmitted via Ethernet over 8 bits, 1% precision given
+    if (cmd != 0) {
+        duty = (uint32_t)((abs(cmd) * 65535UL) / 100UL);
+    }
+	
+	if (cmd > 0) {
+        // Forward
+        __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1, duty);
+        __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_2, 0);
+    }
+    else if (cmd < 0) {
+        // Reverse
+        __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1, 0);
+        __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_2, duty);
+    }
+    else {
+        // Stop
+        __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1, 0);
+        __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_2, 0);
+    }
 }
 
 // Reads the speed of the motor via the encoder
